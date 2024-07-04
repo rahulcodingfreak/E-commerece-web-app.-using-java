80ca325c7be2014bf1c1226cf01b4d743019fbc8
rahulrun <rahulraftaar1516@gmail.com>
2024-07-04 17:08:58 +0530

    Shopping cart

---
+++ src/com/rahul/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.rahul.service;
+
+import java.util.List;
+
+import com.rahul.beans.OrderBean;
+import com.rahul.beans.OrderDetails;
+import com.rahul.beans.TransactionBean;
+
+public interface OrderService {
+
+	public String paymentSuccess(String userName, double paidAmount);
+
+	public boolean addOrder(OrderBean order);
+
+	public boolean addTransaction(TransactionBean transaction);
+
+	public int countSoldItem(String prodId);
+
+	public List<OrderBean> getAllOrders();
+
+	public List<OrderBean> getOrdersByUserId(String emailId);
+
+	public List<OrderDetails> getAllOrderDetails(String userEmailId);
+
+	public String shipNow(String orderId, String prodId);
+}
---
+++ src/com/rahul/service/TransService.java
@@ -0,0 +1,6 @@
+package com.rahul.service;
+
+public interface TransService {
+
+	public String getUserId(String transId);
+}
